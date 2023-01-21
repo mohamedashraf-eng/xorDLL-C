e31ef259abd139a4078caa9cc6b065a5441342e1
MohamedWx <medooka203@gmail.com>
2023-01-21 19:41:28 +0200

    Added license

--- xorDLL/xorDLL_prg.c
+++ xorDLL/xorDLL_prg.c
@@ -5,9 +5,22 @@
  * @version 0.1
  * @date 2023-01-21
  *
- * @copyright Copyright (c) 2023
+ * @copyright Copyright (c) Mohamed Ashraf Wx, 2023
  *
  * @attention
+ * Copyright [2023] [Mohamed Ashraf Wx]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  */
 /*
